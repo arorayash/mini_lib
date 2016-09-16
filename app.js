e7def67e6c06d8f3be9d342f8669e849e8bab1cf
Yash Arora <arorayash.thisisus@gmail.com>
2016-09-16 11:46:57 +0530

    added ejs

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views','./src/views');
+
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-    res.send("hello");
+    res.render("index", {title: "hello", list:['a', 'b']});
 });
 
 app.listen(port, function(err){
